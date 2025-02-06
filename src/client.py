aed2abae8cc603cf5b47a80522e8828b942f95fb
Jordan Muir <jordanhmuir@gmail.com>
2025-02-05 18:09:16 -0800

    added model type as a optional parameter to GoogleClient

--- src/client.py
+++ src/client.py
@@ -7,10 +7,10 @@ from .utils import create_logger
 
 
 class GoogleClient:
-    def __init__(self, api_key: str):
+    def __init__(self, api_key: str, model="gemini-2.0-flash"):
         self.logger = create_logger()
-        self.logger.propagate = False
         self.client = genai.Client(api_key=api_key)
+        self.model = model
         self.music_files = self.parse_music_data()
 
     def parse_music_data(self) -> list:
@@ -30,14 +30,14 @@ class GoogleClient:
 
     def create_response(self, query: str) -> str:
         response = self.client.models.generate_content(
-            model="gemini-2.0-flash", contents=f"{query}"
+            model=self.model, contents=f"{query}"
         )
         self.logger.info("Completed standard text response.")
         return response.text
 
     def create_response_with_audio(self, query: str) -> str:
         response = self.client.models.generate_content(
-            model="gemini-2.0-flash", contents=[f"{query}", self.music_files[0]]
+            model=self.model, contents=[f"{query}", self.music_files[0]]
         )
         self.logger.info("Completed standard response with audio content.")
         return response.text
