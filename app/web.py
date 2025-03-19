9e7ebe2752d8f1f432add73dc1fd0065e19ade8f
Jordan Muir <jordanhmuir@gmail.com>
2025-03-19 15:11:53 -0700

    renamed main llm file

--- app/web.py
+++ app/web.py
@@ -4,7 +4,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 import uvicorn
 
-from llm.mairio_gemini import GeminiClient
+from llm.gemini import GeminiClient
 
 logger = logging.getLogger(__name__)
 
